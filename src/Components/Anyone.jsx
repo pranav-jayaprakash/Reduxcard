230b97eedabbc2d9c5ca2b6a4740a8744be39ea7
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-16 08:40:33 +0530

    update

---
+++ src/Components/Anyone.jsx
@@ -0,0 +1,30 @@
+import React,{useEffect,useContext} from 'react'
+import axios from 'axios';
+import { UserContext } from './Context';
+
+
+function Anyone() {
+
+    const {dispatch} = useContext(UserContext)
+
+    useEffect(()=>{
+        const Dataget = async () => {
+            try {
+               const result = await axios.get('') 
+            } catch (error) {
+                console.log(error);
+                
+            }
+        };
+
+        Dataget()
+
+    },[])
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Anyone
