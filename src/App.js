ed20cce7da3e8703d3cc675bdf0d48a3aff18c12
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-16 11:16:32 +0530

    update task complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Anyone from './Components/Anyone';
 import CardFile from './Components/CardFile';
 
 function App() {
   return (
     <div className="App">
+      <Anyone/>
       <CardFile/>
     </div>
   );
--- src/Components/CardFile.jsx
+++ src/Components/CardFile.jsx
@@ -1,27 +1,42 @@
-import React,{useContext} from 'react'
-import { UserContext } from './Context'
+import React, { useContext,useState } from "react";
+import { UserContext } from "./Context";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from 'react-bootstrap/Button';
+
 
 function CardFile() {
 
-    const {User} = useContext(UserContext)
-    console.log(User,'Dataaaaaaaaaa')
+    const [count, setcount] = useState(10)
 
+    const Adder = ()=>{
+        setcount(count+5)
+    }
+
+    
+  const { User } = useContext(UserContext);
+  
 
   return (
     <div>
-
-        {User.map((dt)=>(
-
-        
-        <h1></h1>
-
+      <Row xs={1} md={2} className="g-4">
+        {User.slice(0, count).map((dt) => (
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>{dt.id}</Card.Title>
+                <Card.Text>{dt.title}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-
-
-
         
+      </Row>
+      <br />
+      <Button variant="dark" onClick={Adder}>Load More</Button>
     </div>
-  )
+  );
 }
 
-export default CardFile
+export default CardFile;
