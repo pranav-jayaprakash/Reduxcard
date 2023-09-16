23254c7eb4f6ad99ee7186eb29d3bd374b7a96d7
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-16 08:45:32 +0530

    update

--- src/Components/CardFile.jsx
+++ src/Components/CardFile.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { UserContext } from './Context'
 
 function CardFile() {
+
+    const {User} = useContext(UserContext)
 
 
   return (
     <div>
 
         
+
+
+
         
     </div>
   )
