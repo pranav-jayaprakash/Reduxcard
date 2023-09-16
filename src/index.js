8c6459ed7d1212a0f6e722c4e5b65ebf2f71b99e
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-16 10:43:52 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserInfoContextProvider } from './Components/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <UserInfoContextProvider>
     <App />
+    </UserInfoContextProvider>
+   
   </React.StrictMode>
 );
 
